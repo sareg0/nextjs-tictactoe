83f7f60dbf5754b8345d6dbd9b68d7deb74c3bfb
Sara Regan <regan.sara@gmail.com>
2022-03-07 09:07:14 +0100

    Add a file to handle data storage for now

--- pages/api/game/index.ts
+++ pages/api/game/index.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Game } from '../../../lib/tictactoe'
+import * as fs from 'fs'
 
 type Data = {
   game: Game
@@ -11,12 +12,24 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   const game = new Game()
+  fs.readFile('<entire path to json file here>', 'utf8', (err, jsonString) => {
+    if (err) {
+      console.log('file read failed:', err)
+      return
+    }
+    try {
+      const games = JSON.parse(jsonString)
+      console.log('games', games)
+
+    } catch(e) {}
+    console.log('file data:', jsonString)
+  })
   res.status(200).json({ game })
 }
 
-// POst to game, a get a new game, with an ID
-// Post to game/:id/addMove to make a 
+//Post to game/new brings back a new game, but also stores that game in a JSON file.
+// GET a new game, by an ID
+// UPDATE to game/:id to make a
+// will need to write to a file. Local memory will be wiped
 
 // TODO, get the tests back in
-// fix the TS errors
-// 
